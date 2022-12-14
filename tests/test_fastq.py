14d7320bba86f9b9a813f320c15a432197e04aa3
ckaipf <ckaipf@posteo.de>
2022-12-14 14:38:08 +0100

    update function name

--- tests/test_fastq.py
+++ tests/test_fastq.py
@@ -2,7 +2,7 @@ from fafova.fastq import fastq_validation
 import pytest
 from fixtures import negatives_fastq, positives_fastq
 
-def test_regify_file():
+def test_fastq_validation():
     for file in negatives_fastq:
         with pytest.raises(SystemExit) as e:
             fastq_validation(file)
